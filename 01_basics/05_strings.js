33cfd550fcd436fef7d0c2485d0ec72c3ba72dda
Yashasvi Matta <54911834+yashasvimatta@users.noreply.github.com>
2024-04-11 16:57:15 +0000

    strings completed

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,8 +1,26 @@
 const name = "yashasvi"
 const age = 23
 
-console.log(`Hello my name is ${name} and my age is ${age}`);
+console.log(`Hello my name is ${name} and my age is ${age}`);           //backticks
 
-const webName = new String('devYashasvi')
+const webName = new String('dev-Yashasvi')
 console.log(webName[3])                                   // outputs Y
 console.log(webName.__proto__)                            // outputs {}
+
+
+console.log(webName.length)
+// console.log(webName.toLowerCase())
+
+const newString = webName.substring(0,4)
+// console.log(newString)
+
+const anotherString = webName.slice(-5,2)
+console.log(anotherString)
+
+const onemorestring = "    yashasvi        "
+console.log(onemorestring.trim())
+
+const url = 'https://yashasvi%20matta'
+a= url.replace("%20","_")
+
+console.log(a.split("_"))
