48ca98882dfe088d91058d63ac59427eac8088f0
Yashasvi Matta <54911834+yashasvimatta@users.noreply.github.com>
2024-04-22 02:08:44 +0000

    arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,14 +1,32 @@
  // array creates shallow copies (same reference , changes in orignal copy)
+const  arr = [1,2,3,4,5]
+ console.log(arr)
 
- const  arr = [1,2,3,4,5]
-//  console.log(arr)
+const myArray = new Array(1,2,3,4,5)
 
-const myArray = new Array(1,2,3,4,5,6)
-
-console.log(myArray(3))                            // access spefic element . outputs 4
+console.log(myArray[3])                            // access spefic element . outputs 4
 
 
 // Array methods
 myArray.push(6)
 myArray.push(7)
+console.log(myArray)
 myArray.pop()
+console.log(myArray)   
+
+myArray.unshift(9)                              // outputs array with 9 as first element shift all values of array to right , time consuming method
+myArray.shift()                                 // removes 9
+console.log(myArray)            
+const newArr = myArray.join()
+
+console.log(newArr)
+
+const myn1 = myArray.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArray);
+
+
+const myn2 = myArray.splice(1, 3)               // solice changes the original array 
+console.log("C ", myArray);
+console.log(myn2);
